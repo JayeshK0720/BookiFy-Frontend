1981002666be25cf138c8854657522b25093f410
JayeshK0720 <jaykotkar07@gmail.com>
2025-02-26 18:08:47 +0530

    first commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     // return "http://localhost:5000"
-    return "booki-fy-backend.vercel.app"
+    return "https://booki-fy-backend.vercel.app/"
 }
 
 export default getBaseUrl;
